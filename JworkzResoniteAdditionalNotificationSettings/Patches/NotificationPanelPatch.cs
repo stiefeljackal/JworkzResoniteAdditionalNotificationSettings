fe079047049e33f42837840cf37ea13ed44f23f8
StiefelJackal <stiefel@jackalworkz.net>
2023-10-05 12:24:54 -0400

    feat: add "is online" cooldown

--- JworkzResoniteAdditionalNotificationSettings/Patches/NotificationPanelPatch.cs
+++ JworkzResoniteAdditionalNotificationSettings/Patches/NotificationPanelPatch.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 using FrooxEngine;
 using SkyFrost.Base;
 using HarmonyLib;
 using ModConfig = JworkzResoniteMod.JworkzResoniteAdditionalNotificationSettings;
 using MessageType = SkyFrost.Base.MessageType;
+using OnlineStatus = SkyFrost.Base.OnlineStatus;
 
 namespace JworkzResoniteMod.Patches;
 
 [HarmonyPatch(typeof(NotificationPanel))]
 public static class NotificationPanelPatch
 {
+    private static readonly ConcurrentDictionary<string, DateTimeOffset> _userIsOnlineNotificationTimestampPairs = new ();
+
     [HarmonyPrefix]
     [HarmonyPatch("Messages_OnMessageReceived")]
     public static bool MessagesOnMessageReceivedPrefix(Message message)
@@ -37,6 +41,28 @@ public static class NotificationPanelPatch
     [HarmonyPatch("ContactStatusUpdated")]
     public static bool ContactStatusUpdatePrefix(ContactData contact)
     {
-        return ModConfig.AllowIsOnlineNotifications;
+        if (contact.Contact.IsSelfContact) { return true; }
+
+        var onlineStatus = contact.CurrentStatus.OnlineStatus.GetValueOrDefault();
+        if (onlineStatus != OnlineStatus.Online) { return true; }
+
+        if (!ModConfig.AllowIsOnlineNotifications) {  return false; }
+
+        var userId = contact.Contact.ContactUserId;
+        var hasTimestampEntry = _userIsOnlineNotificationTimestampPairs.ContainsKey(userId);
+        var canShowNotification = true;
+        
+        if (hasTimestampEntry)
+        {
+            var hasRetrievedTimestamp = _userIsOnlineNotificationTimestampPairs.TryGetValue(userId, out DateTimeOffset lastNotificationSent);
+            canShowNotification = hasRetrievedTimestamp && DateTimeOffset.UtcNow.Subtract(lastNotificationSent).Seconds >= ModConfig.IsOnlineDelayInSeconds;
+        }
+
+        if (canShowNotification)
+        {
+            _userIsOnlineNotificationTimestampPairs.AddOrUpdate(userId, DateTimeOffset.UtcNow, (_, _2) => DateTimeOffset.UtcNow);
+        }
+
+        return canShowNotification;
     }
 }
